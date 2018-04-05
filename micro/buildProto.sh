2911e8de7b01384d93638d20e21fbb714f296007
Pierre Poissinger <Pierre.Poissinger@ordina.be>
2018-04-05 19:04:55 +0200

    Added some go get

--- micro/buildProto.sh
+++ micro/buildProto.sh
@@ -1,2 +1,5 @@
 #!/bin/sh
+# prereq: brew install protobuf
+go get -v github.com/golang/protobuf/protoc-gen-go && go get -v github.com/micro/protoc-gen-micro 
+
 protoc -I$GOPATH/src --go_out=plugins=micro:${GOPATH}/src $(pwd)/proto/holiday/holiday.proto
